c1984384834c00ea0b6f5abce3258ae7b4d1dec6
RNLFoof <z.zablotsky@gmail.com>
2025-01-17 18:25:32 -0500

    feature: transfering works!

--- main.lua
+++ main.lua
@@ -1,4 +1,81 @@
-SMODS.Seal {
+sealCalculateFunction = function(self, card, context)
+    function cardStr(card)
+        if card == nil then
+            return "[??? ???]"
+        end
+        if card.base == nil then
+            return "[???]"
+        end
+        return string.format("[%d%s $s]", card.base.id, card.base.suit, card.seal)
+    end
+
+    print(cardStr(card), "Evaluating seal")
+    -- main_scoring context is used whenever the card is scored
+    if context.main_scoring and context.cardarea == G.play then
+        -- get the index of the card within the scoring cards
+        print(cardStr(card), "correct context")
+        
+        -- So, all the cards are evaluated before the animations play.
+        -- This means that in order to have the seal visibly transfer between multiple cards,
+        -- Each card needs to find out if it *will* be transfered to them.
+        cardWithSeal = card
+        passesBetween = {}
+        for i, checkingCard in pairs(context.scoring_hand) do
+            if checkingCard == cardWithSeal then
+                cardIndex = i
+                if cardIndex ~= #context.scoring_hand then
+                    nextCardIndex = cardIndex+1
+                    nextCard = context.scoring_hand[nextCardIndex]
+                    
+                    -- if the next card is sequential and sealess, pass the seal on
+                    print(cardStr(cardWithSeal), "debug vars")
+                    print(cardStr(cardWithSeal), "four", 4)
+                    print(cardStr(cardWithSeal), "rank", cardWithSeal.rank)
+                    print(cardStr(cardWithSeal), "next card rank", nextCard.rank)
+                    print(cardStr(cardWithSeal), "real rank", nextCard.base.id)
+                    if cardWithSeal.base.id + 1 == nextCard.base.id and nextCard.seal == nil then
+                        print(cardStr(cardWithSeal), "confirmed, passing to", cardStr(nextCard))
+                        local cardWithSealInEvent = context.scoring_hand[cardIndex]
+                        local nextCardInEvent = context.scoring_hand[nextCardIndex]
+                        spacing = 0.001
+
+                        G.E_MANAGER:add_event(Event({delay=spacing,trigger="after",func = function()
+                            print(cardStr(cardWithSealInEvent), "playing animation of passing to", cardStr(nextCardInEvent))
+                            cardWithSealInEvent:flip()
+                            return true
+                        end;}))
+                                    
+                        G.E_MANAGER:add_event(Event({delay=spacing,trigger="after",func = function()
+                                    nextCardInEvent:flip()
+                                    return true
+                        end;}))
+                        G.E_MANAGER:add_event(Event({delay=spacing,trigger="after",func = function()
+                                    cardWithSealInEvent:set_seal(nil, true, true)
+                                    nextCardInEvent:set_seal('conj_indigo', true, true)
+                                    return true
+                        end;}))
+                        G.E_MANAGER:add_event(Event({delay=spacing,trigger="after",func = function()
+                                    cardWithSealInEvent:flip()
+                                    return true
+                        end;}))
+                        G.E_MANAGER:add_event(Event({delay=spacing,trigger="after",func = function()
+                                    nextCardInEvent:flip()
+                                    return true
+                        end;}))
+                        sealCalculateFunction(self, nextCardInEvent, context)
+                            
+                        break
+                    end
+                end
+            end
+        end
+        
+        -- if this is the last card, bail, otherwise take note
+        print(cardStr(card), "3")
+    end
+end
+
+indigoSeal = SMODS.Seal {
     name = "modded-Seal",
     key = "indigo",
     badge_colour = HEX("5729e0"),
@@ -10,14 +87,12 @@ SMODS.Seal {
         name = 'Indigo Seal',
         -- This could really use more concise phrasing...
         text = {
-            "Travels upwards between",
-            "scored cards when",
-            "they're directly sequential",
-            "in both position and rank.",
+            "Travels upwards between scored",
+            "{C:attention}position & rank{} sequential cards.",
             "Upon reaching an Ace,",
             "is removed and creates a",
             "{C:dark_edition}negative{} {C:attention}Stars{}.",
-            "{C:inactive}Ex: A[JQ]2K, 78[456]"
+            "{C:inactive}Ex: A[J to Q]2K, 78[4 to 5 to 6]"
         }
     },
     loc_vars = function(self, info_queue)
@@ -28,53 +103,7 @@ SMODS.Seal {
 
     -- self - this seal prototype
     -- card - card this seal is applied to
-    calculate = function(self, card, context)
-        function cardStr(card)
-            return string.format("[%d%s]", card.base.id, card.base.suit)
-        end
-
-        print(cardStr(card), "1")
-        -- main_scoring context is used whenever the card is scored
-        if context.main_scoring and context.cardarea == G.play then
-            -- get the index of the card within the scoring cards
-            print(cardStr(card), "2")
-            cardIndex = nil
-            for i, checkingCard in pairs(context.scoring_hand) do
-                if checkingCard == card then
-                    cardIndex = i
-                    break
-                end
-            end
-            
-            -- if this is the last card, bail, otherwise take note
-            print(cardStr(card), "3")
-            if cardIndex ~= #context.scoring_hand then
-                nextCard = context.scoring_hand[cardIndex+1]
-                
-                -- if the next card is sequential and sealess, pass the seal on
-                print(cardStr(card), "4")
-                print(cardStr(card), 4)
-                print(cardStr(card), card.rank)
-                print(cardStr(card), nextCard.rank)
-                print(cardStr(card), nextCard.base.id)
-                print(cardStr(card), nextCard.sealess)
-                if card.base.id + 1 == nextCard.base.id and nextCard.seal == nil then
-                    G.E_MANAGER:add_event(
-                        Event({
-                            trigger = 'after',
-                            delay = 0.1,
-                            func = function()
-                                print(cardStr(card), "5")
-                                nextCard:set_seal('conj_indigo', nil, false)
-                                card:set_seal(nil, true, true)
-                                return true
-                            end
-                        }))
-                end
-
-            end
-        end
-    end,
+    calculate = sealCalculateFunction
 }
 
 SMODS.Atlas {
